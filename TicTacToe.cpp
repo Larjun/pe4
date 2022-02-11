3425b12963de9fa5066222470bf8098ec68f540b
Arjun Lakshmi Narasimhan <larjun01@cu-engr2-1-15-10.201.58.12.int.colorado.edu>
2022-02-11 10:59:56 -0700

    Got the game working

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -3,65 +3,44 @@
 
 using namespace std;
 
-struct Location {
-	int x;
-	int y;
-};
-
 class TicTacToe {
     private:
-        bool playerSq[3][3];
-        bool enemySq[3][3];
         int board[3][3];
+        int side;
 
     public:
         TicTacToe() {
             for(int x = 0; x < 3; x++) {
                 for(int y = 0; y < 3; y++) {
-                    playerSq[x][y] = false;
-                    enemySq[x][y] = false;
-                    board[x][y] = 0;
+                    board[x][y] = -1;
                 }
             }
+
+            side = 0;
         }
 
         void testBoard() {
-            board[0][0] = 1;
-            playerSq[0][0] = true;
+            board[0][0] = 0;
+            board[0][1] = 1;
+            board[0][2] = 0;
 
-            board[0][1] = 2;
-            enemySq[0][1] = true;
+            board[1][0] = 1;
+            board[1][1] = 0;
+            board[1][2] = 1;
 
-            board[0][2] = 1;
-            playerSq[0][2] = true;
-
-            board[1][0] = 2;
-            enemySq[1][0] = true;
-
-            board[1][1] = 1;
-            playerSq[1][1] = true;
-
-            board[1][2] = 2;
-            enemySq[1][2] = true;
-
-            board[2][0] = 1;
-            playerSq[2][0] = true;
-
-            board[2][1] = 2;
-            enemySq[2][1] = true;
-
-            board[2][2] = 1;
-            playerSq[2][2] = true;
+            board[2][0] = 0;
+            board[2][1] = 1;
+            board[2][2] = 0;
         }
         
         void printBoard() {
             for(int x = 0; x < 3; x++) {
                 for(int y = 0; y < 3; y++) {
                     switch(board[x][y]) {
-                        case 1:
+                        case 0:
                             cout << "X";
                             break;
-                        case 2:
+                        case 1:
                             cout << "O";
                             break;
                         default:
@@ -75,39 +54,91 @@ class TicTacToe {
             }
         } 
 
-        Location GetPlayerChoice() {
-            bool go = true;
-            string x;
-            string y;
-            while(go) {
-                x = "";
-                cout << endl << "Enter a row to move to 0-2: ";
-                cin >> x;
-                if (x == "0" || x == "1" || x == "2") {
-                    go = false;
+        bool placeMarker(int side, int x, int y) {
+            if(board[x][y] != -1) {
+                if(board[x][y] == side) {
+                    cout << "You already placed the marker there, please enter another coordinates";
+                } else {
+                    cout << "The enemy already placed the marker there, please enter another coordinates";
                 }
+
+                return false;
+            } else {
+                board[x][y] = side;
+                return true;
             }
-            go = true;
-            while(go) {
-                y = "";
-                cout << endl << "Enter a column to move to 0-2: ";
-                cin >> y;
-                if (y == "0" || y == "1" || y == "2") {
-                    go = false;
-                }
-            }
-            Location loc;
-            loc.x = stoi(x);
-            loc.y = stoi(y);
-            return loc;
         }
 
-        void placeMarker(int side);
+        void GetPlayerChoice() {
+            bool go = true;
+            bool placable = false;
+            string x;
+            string y;
 
-        int checkGameEnd();
-};
+            while(!placable) {
+                if(side == 0) {cout << "X's turn";} 
+                else {cout << "O's turn";}
+
+                while(go) {
+                    x = "";
+                    cout << endl << "Enter a row to move to 0-2: ";
+                    cin >> x;
+                    if (x == "0" || x == "1" || x == "2") {
+                        go = false;
+                    }
+                }
+                go = true;
+                while(go) {
+                    y = "";
+                    cout << endl << "Enter a column to move to 0-2: ";
+                    cin >> y;
+                    if (y == "0" || y == "1" || y == "2") {
+                        go = false;
+                    }
+                }
+
+                placable = placeMarker(side, stoi(x), stoi(y));
+            }
+
+
+            side = (side+1)%2;
+        }
+
+        
+
+        int checkGameEnd() {
+            int winner = 0;
+            for(int x = 0; x < 3; x++) {
+                if(board[x][0] == board[x][1] && board[x][1] == board[x][2]) {
+                    winner =  board[x][0] + 1;
+                }
+            }
+
+            for(int y = 0; y < 3; y++) {
+                if(board[0][y] == board[1][y] && board[1][y] == board[2][y]) {
+                    winner =  board[0][y] + 1;
+                }
+            }
+
+            if(board[0][0] == board[1][1] && board[1][1] == board[2][2]) {
+                winner = board[1][1] + 1;
+            }
+            
+            if(board[2][0] == board[1][1] && board[1][1] == board[0][2]) {
+                winner = board[1][1] + 1;
+            }
+
+            return winner;
+        }
+};      
 
 int main() {
     TicTacToe game = TicTacToe();
-    game.GetPlayerChoice();
+    while(!game.checkGameEnd()) {
+        game.GetPlayerChoice();
+        game.printBoard();
+    }
+
+    if(game.checkGameEnd() == 1) {cout << "X wins the game" << endl;}
+    else {cout << "O wins the game" << endl;}
 }
